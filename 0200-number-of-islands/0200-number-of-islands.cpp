38090bc4a00f2b7a3b21c233721c19fe25170160
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-06 23:45:54 +0530

    Time: 32 ms (29.15%), Space: 24.9 MB (14.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    void bfs(vector<vector<char>>& grid,vector<vector<int>>&vis,int i,int j){
+     void bfs(vector<vector<char>>& grid,int i,int j,vector<vector<int>>&vis){
         int m=grid.size();
         int n=grid[0].size();
         vis[i][j]=1;
@@ -24,19 +24,19 @@ class Solution {
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
+        int c=0;
         int m=grid.size();
         int n=grid[0].size();
-        int c=0;
         vector<vector<int>>vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
+                if(grid[i][j]=='1'&&!vis[i][j]){
                     c++;
-                    bfs(grid,vis,i,j);
+                    bfs(grid,i,j,vis);
                 }
             }
+
         }
         return c;
-        
     }
 };
