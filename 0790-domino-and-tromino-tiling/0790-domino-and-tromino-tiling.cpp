d45f80d89c91b32121912dcc0a8cb295224ec2de
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-07 02:52:52 +0530

    Time: 0 ms (100%), Space: 9.2 MB (25.3%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,14 +1,18 @@
 class Solution {
-    public int numTilings(int n) {
-        int mod= 1_000_000_007;
+public:
+    int numTilings(int n) {
+        int mod = 1000000007;
         if (n <= 1) return 1;
         if (n == 2) return 2;
         if (n == 3) return 5;
-        long[] dp = new long[n + 1];
-        dp[0] = 1; dp[1] = 1; dp[2] = 2; dp[3] = 5;
+        vector<long long> dp(n + 1);
+        dp[0] = 1; 
+        dp[1] = 1; 
+        dp[2] = 2; 
+        dp[3] = 5;
         for (int i = 4; i <= n; i++) {
-            dp[i] = (2 * dp[i - 1] % mod + dp[i - 3]) % mod;
+            dp[i] = (2LL * dp[i - 1] % mod + dp[i - 3]) % mod;
         }
-        return (int) dp[n];
+        return dp[n];
     }
-}
+};
