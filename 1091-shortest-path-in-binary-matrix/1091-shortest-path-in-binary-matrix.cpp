3ea306c10a304d7f9b7ba1d6355495653fdc32b9
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-09 01:37:41 +0530

    Time: 17 ms (65.34%), Space: 26.4 MB (34.2%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,37 +2,30 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        int m = grid[0].size();
-        if(grid[0][0]!=0 || grid[n-1][m-1]!=0){
-            return -1;
-        }
-         if(grid[0][0]==0 && n==1 && m==1){
-            return 1;
-        }
-        vector<vector<int>> dist(n,vector<int>(m,1e7));
-        dist[0][0]=0;
-        queue<pair<int,pair<int,int>>> q;
-        q.push({1,{0,0}});
-        vector<int> drow = {0,1,-1,0,1,-1,1,-1};
-        vector<int> dcol = {1,0,0,-1,1,1,-1,-1};
-         while(!q.empty()){
-            auto it = q.front();
+        if(grid[0][0]==1 ||grid[n-1][n-1]==1) return -1;
+        queue<pair<int,pair<int,int>>>q;
+        q.push({0,{0,0}});
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        vis[0][0]=1;
+        int dr[]={-1,0,1,0,-1,1,-1,1};
+        int dc[]={0,1,0,-1,-1,1,1,-1};
+        while(!q.empty()){
+             int d = q.front().first;
+            int r = q.front().second.first;
+            int c = q.front().second.second;
             q.pop();
-            int dis = it.first;
-            int row = it.second.first;
-            int col = it.second.second;
-            for(int i=0; i<8; i++){
-                int nrow = row+drow[i];
-                int ncol = col+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && dis+1<dist[nrow][ncol] && grid[nrow][ncol]==0){
-                    dist[nrow][ncol] = dis+1;
-                    if(nrow == n-1 && ncol == m-1){
-                        return dis+1;
-                    }
-                    q.push({dis+1,{nrow,ncol}});
+            if(r==n-1 &&c==n-1)return d+1;
+            for(int i=0;i<8;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 &&nr<n&&nc>=0 &&nc<n && !vis[nr][nc]&& grid[nr][nc]==0){
+                    q.push({d+1,{nr,nc}});
+                    vis[nr][nc]=1;
+
                 }
             }
         }
         return -1;
+
     }
 };
