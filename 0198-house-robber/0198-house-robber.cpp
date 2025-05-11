a5582029e38933ea7952a57e7ab83ee36c971825
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-11 19:07:47 +0530

    Time: 10 ms (0.41%), Space: 10.1 MB (79.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,20 @@
 class Solution {
+int dp[101];
 private:
 int Recursion(vector<int>&nums,int idx,int n){
     if(idx>=n)return 0;
-
+    if(dp[idx]!=-1)return dp[idx];
     int take=nums[idx]+Recursion(nums,idx+2,n);
     int notTake=Recursion(nums,idx+1,n);
-    return max(take, notTake);
+    return dp[idx]=max(take, notTake);
 
 
 }
 
 public:
     int rob(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        for(auto i:dp)cout<<i<<endl;
         return Recursion(nums,0,nums.size());
         
     }
