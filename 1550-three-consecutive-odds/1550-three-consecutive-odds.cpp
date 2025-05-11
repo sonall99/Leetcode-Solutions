01b74264b6da0b8fe635fa4f4c4d0de67883c51e
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-11 17:26:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int arr_size=arr.size();
+        int c=0;
+        for(int i=0;i<arr_size;i++){
+            if(c>=3) return true;
+            if(arr[i]%2==0) c=0;
+            else{
+                c++;
+            }
+        }
+        return false;
+        
+    }
+};
