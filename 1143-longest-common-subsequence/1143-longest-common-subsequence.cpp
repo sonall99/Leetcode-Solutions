e0009c42a9555e0d23a2d0c31185e4d5c2ca361c
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-12 15:19:33 +0530

    Time: 51 ms (6.69%), Space: 27.5 MB (27.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,32 +1,28 @@
 class Solution {
-vector<vector<int>>dp;
-int solve(string s,string t,int i,int j,int n,int m){
-    if(i>=n || j>=m){
-        // if(s[i]==s[j])return 1;
-        // else 
-        return 0;
-    }
-    if(dp[i][j]!=-1)return dp[i][j];
-    if(s[i]==t[j]){
-        return dp[i][j]= 1+solve(s,t,i+1,j+1,n,m);
-    }
-    else{
-    // int case1=solve(s,t,i+1,j,n,m);
-    // int case2=solve(s,t,i,j+1,n,m);
-    return dp[i][j]= max(solve(s,t,i+1,j,n,m),solve(s,t,i,j+1,n,m));
-    
+    vector<vector<int>> dp;
 
-    }
-    // return 0;
+    int solve(const string &s, const string &t, int i, int j, int n, int m) {
+        if (i >= n || j >= m) return 0;
 
-}
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s[i] == t[j]) {
+            return dp[i][j] = 1 + solve(s, t, i + 1, j + 1, n, m);
+        } else {
+            int case1 = solve(s, t, i + 1, j, n, m);
+            int case2 = solve(s, t, i, j + 1, n, m);
+            return dp[i][j] = max(case1, case2);
+        }
+    }
 
 public:
     int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
 
-        dp.assign(text1.size(),vector<int>(text2.size(),-1));
-        return solve(text1,text2,0,0,text1.size(),text2.size());
-      
-        
+        // Initialize dp table with -1
+        dp.assign(n, vector<int>(m, -1));
+
+        return solve(text1, text2, 0, 0, n, m);
     }
 };
