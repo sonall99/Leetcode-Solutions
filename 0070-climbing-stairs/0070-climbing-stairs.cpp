47fee1d2269f71e51adab7f371a184ffdb5d5766
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-12 14:41:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+int solve(int n){
+    if(n==0)return 1;
+    if(n<0)return 0;
+    return solve(n-1)+solve(n-2);
+}
+public:
+    int climbStairs(int n) {
+        return solve(n);
+        
+    }
+};
