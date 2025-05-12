9b82560c4f8dc2f0b0e463d3ad78b69677c233d1
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-12 23:41:14 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,14 @@
 class Solution {
+int dp[50];
 int solve(int n){
     if(n==0)return 1;
     if(n<0)return 0;
-    return solve(n-1)+solve(n-2);
+    if(dp[n]!=-1)return dp[n];
+    return  dp[n]=solve(n-1)+solve(n-2);
 }
 public:
     int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
         return solve(n);
         
     }
