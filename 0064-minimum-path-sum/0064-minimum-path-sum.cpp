0ce153e1661c5f1a4c2c7302216a110f11c63242
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-18 14:44:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,24 +1,18 @@
 class Solution {
-private:
-int dp[201][201];
-private:
-int Solve(vector<vector<int>>&triangle,int row,int col,int n,int m){
-    // if(row>=n || col >=m)return 1e9;
-    if(row == n-1 && col==m-1)return triangle[row][col];
-    if(dp[row][col] !=-1)return dp[row][col];
-    int case1=1e9;
-    int case2=1e9;
-    if(row+1<n)
-     case1= Solve(triangle,row+1,col,n,m);
-     if(col+1<m)
-     case2= Solve(triangle,row,col+1,n,m);
-    return dp[row][col]=triangle[row][col]+min(case1,case2);
+    int dp[201][201];
+    int min_path(int r,int c,vector<vector<int>>& grid,int m ,int n){
+        if(dp[r][c]!=-1) return dp[r][c];
+        int case1=INT_MAX,case2=INT_MAX;
+        if(r+1<m) int case1=min_path(r+1,c,grid,m,n);
+        if(c+1<n) int case2=min_path(r,c+1,grid,m,n);
+        return dp[r][c]=min(case1,case2);
 
-}
+    }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         memset(dp,-1,sizeof(dp));
-        return Solve(grid,0,0,grid.size(),grid[0].size());
-        
+        int m=grid.size();
+        int n=grid[0].size();
+        return min_path(0,0,grid,m,n);
     }
 };
