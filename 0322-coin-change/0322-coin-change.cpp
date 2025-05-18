56c1859c82e1276da8147f679025fefd89cacacd
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-18 13:42:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,23 @@
 class Solution {
-    int dp[13][10001];
-    int n;
-    int rec(int ind,vector<int>& coins, int amount){
-        if(amount==0) return 0;
-        if(ind>=n &&amount!=0) return INT_MAX-1;
-        int res=0;
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        if(coins[ind]>amount){
-            int not_take=0+rec(ind+1,coins,amount);
-            res=not_take;
-        }
-        else if(coins[ind]<=amount){
-            int take=1+rec(ind,coins,amount-coins[ind]);
-            int not_take=0+rec(ind+1,coins,amount);
-            res=min(take,not_take);
-        }
-        return dp[ind][amount]=res;
 
-        
-    }
 public:
+int dp[13][10001];
+int check(int ind,vector<int>& coins, int amount,int n){
+    if(amount==0) return 0;
+    if(ind>=n && amount !=0) return INT_MAX-1;
+    if(dp[ind][amount]!=-1) return dp[ind][amount];
+    int take=0;
+    if(coins[ind]<=amount) {
+        take=1+check(ind,coins,amount-coins[ind],n);
+    }
+    int not_take= check(ind+1,coins,amount,n);
+    return dp[ind][amount]=min(take,not_take);
+
+}
+
     int coinChange(vector<int>& coins, int amount) {
         memset(dp,-1,sizeof(dp));
-        n=coins.size();
-        int ans= rec(0,coins,amount);
+        int ans= check(0,coins,amount,coins.size());
         return (ans==INT_MAX-1)?-1:ans;
     }
 };
