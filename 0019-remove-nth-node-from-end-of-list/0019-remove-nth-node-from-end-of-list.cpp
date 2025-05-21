1aaaf83ecc59512d73d80c67c12160a7f69b1e5b
SONAL SINGH <156330222+sonall99@users.noreply.github.com>
2025-05-21 10:03:31 +0530

    Time: 3 ms (1.94%), Space: 15 MB (25.15%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -15,20 +15,18 @@ public:
         ListNode*fast=head;
         for(int i=0;i<n;i++){
             fast=fast->next;
-
         }
-        if (fast == NULL)return head->next;
-        while(fast->next!=NULL){
+        if(fast==NULL) return head->next;
+        while(fast->next){
             slow=slow->next;
             fast=fast->next;
 
         }
         ListNode*del=slow->next;
         slow->next=slow->next->next;
+        del->next=nullptr;
         delete(del);
         return head;
 
-
-        
     }
 };
